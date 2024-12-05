fa69bb585a0fac79578cc21c0126aec07341cbe4
adanguerra1990 <aj90guerra@gmail.com>
2024-12-05 17:00:21 -0400

    Configurar app.js para manejar la configuración de la aplicación y la inicialización del middleware.

---
+++ app.js
@@ -0,0 +1,40 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const blogRouter = require('./controllers/blogs')
+const { unknownEndpoint, errorHandler } = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
+
+logger.info('Conectando...', config.MONGODB_URI)
+
+const url = process.env.MONGODB_URI
+
+logger.info('Conectando...', url)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Conctado a MongoDB')
+  })
+  .catch((error) => {
+    logger.info('Error al conectar con MongoDB', error)
+  })
+
+app.use(express.json())
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) => {
+  res.status(200).send('¡Backend en funcionamiento!')
+})
+
+app.use('/api/blogs', blogRouter)
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+module.exports = app
