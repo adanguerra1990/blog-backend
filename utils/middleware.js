287ca94c84649b3cc8220b0f2dbd74fc56ca0ddb
adanguerra1990 <aj90guerra@gmail.com>
2024-12-17 17:01:01 -0400

    Middleware para extraer y verificar el token

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -14,7 +16,32 @@ const errorHandler = (error, request, response, next) => {
   }
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  const token = request.token
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const user = await User.findById(decodedToken.id)
+  if (user) {
+    request.user = user
+  } else {
+    return response.status(401).json({ error: 'User not found' })
+  }
+  next()
+}
+
 module.exports = {
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
+  userExtractor,
 }
