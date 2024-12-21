1a9931e39486b769dc6d7c4bbd351cbca767ba87
adanguerra1990 <aj90guerra@gmail.com>
2024-12-21 08:43:42 -0400

    Añadiendo referencia de usuario en el esquema de blog y relación de blogs en el esquema de usuario

--- models/blog.js
+++ models/blog.js
@@ -17,6 +17,10 @@ const blogSchema = new mongoose.Schema({
     type: Number,
     require: 0,
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 })
 
 blogSchema.set('toJSON', {
