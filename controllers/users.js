f64c4e26f274504273ea56ff274d1e4f314e77fa
adanguerra1990 <aj90guerra@gmail.com>
2025-01-27 15:06:45 -0400

    Añadiendo población del campo 'blogs' en las rutas de obtención de usuarios para incluir detalles de los blogs

--- controllers/users.js
+++ controllers/users.js
@@ -20,14 +20,23 @@ userRouter.post('/', async (request, response) => {
 })
 
 userRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  })
 
-  console.log('name', users)
   response.json(users)
 })
 
 userRouter.get('/:id', async (request, response) => {
-  const user = await User.findById(request.params.id)
+  const user = await User.findById(request.params.id).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  })
 
   if (user) {
     response.json(user)
